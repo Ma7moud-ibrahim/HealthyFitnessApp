f081edb9d4794c1fa9dcd2f31319f49ec289b4de
Ali <alialashwall14@gmail.com>
2024-09-06 22:40:49 +0300

    remove converters until being implemented

--- app/src/main/java/com/example/healthyfitness/data/data_source/local/UserDatabase.kt
+++ app/src/main/java/com/example/healthyfitness/data/data_source/local/UserDatabase.kt
@@ -4,8 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [], version = 1)
-@TypeConverters(Converters::class)
+//@Database(entities = [], version = 1)
+//@TypeConverters(Converters::class)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
