5883574b313de517079faecbe6df71829c7a9f52
Ali <alialashwall14@gmail.com>
2024-09-06 22:31:09 +0300

    setup navigation

--- app/src/main/java/com/example/healthyfitness/presentation/MainActivity.kt
+++ app/src/main/java/com/example/healthyfitness/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.healthyfitness
+package com.example.healthyfitness.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.healthyfitness.ui.theme.HealthyFitnessTheme
+import com.example.healthyfitness.presentation.theme.HealthyFitnessTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +26,9 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+//                    AppNavHost()
                 }
+
             }
         }
     }
