7207329f659df5decc044749101ee6b81fa2c539
Ali <alialashwall14@gmail.com>
2024-09-06 22:29:02 +0300

    setup dependence injection

---
+++ app/src/main/java/com/example/healthyfitness/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.healthyfitness
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/example/healthyfitness/di/NetworkModule.kt
@@ -0,0 +1,9 @@
+package com.example.healthyfitness.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule
