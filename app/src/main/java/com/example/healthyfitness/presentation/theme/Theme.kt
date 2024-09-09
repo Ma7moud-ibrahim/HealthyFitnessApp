7d09d7a200f04713960edc0e0ac1c9d789b763cc
menna <94606987+Menna120@users.noreply.github.com>
2024-09-09 20:35:33 +0300

    modify MaterialTheme to use the fitnessShapes

--- app/src/main/java/com/example/healthyfitness/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/healthyfitness/presentation/theme/Theme.kt
@@ -8,13 +8,13 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = LemonGreen,
     secondary = DarkGreen,
     tertiary = White,
+    outline = LightGray,
 
 //     Other default colors to override
     background = LightBlack,
@@ -28,6 +28,7 @@ private val LightColorScheme = lightColorScheme(
     primary = LemonGreen,
     secondary = DarkGreen,
     tertiary = White,
+    outline = LightGray,
 
 //     Other default colors to override
     background = LightBlack,
@@ -58,6 +59,7 @@ fun HealthyFitnessTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
+        shapes = fitnessShapes,
         typography = Typography,
         content = content
     )
